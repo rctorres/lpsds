de0c70a42baebb8f540730d2b1abd34fbea154f6
Rodrigo Coura Torres <torres.rc@gmail.com>
2022-03-16 16:37:55 -0300

    Changed unique name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="fast-ds",
+    name="rct-fast-ds",
     version="0.0.1",
     author="Rodrigo Coura Torres",
     author_email="torres.rc@gmail.com",
