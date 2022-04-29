6cfc302f005f87b53f7841f1980ad46c7951d675
Rodrigo Coura Torres <torres.rc@gmail.com>
2022-04-29 14:10:53 -0300

    Added more pre proc classes

--- src/lpsds/preprocessing.py
+++ src/lpsds/preprocessing.py
@@ -89,3 +89,109 @@ def get_itb_range_averages(cat_val, is_integer=False):
         ])
 
     return np.nanmean(vec)
+
+
+class RangeAverage:
+    def __init__(self, is_integer, dtype=np.float64, inplace=True):
+        self.is_integer = is_integer
+        self.dtype = dtype
+        self.inplace = inplace
+    
+    def fit(self, X, y=None, **kwargs):
+        return None
+    
+    def transform(self, X, **kwargs):
+        if not self.inplace: X = X.copy()
+        for c in X.columns:
+            X[c] = X[c].apply(get_itb_range_averages, is_integer=self.is_integer).astype(self.dtype)
+        return X
+    
+    def fit_transform(self, X, y=None, **kwargs):
+        return self.transform(X)
+
+    
+class BooleanEncode:
+    def __init__(self, boolean_map, inplace=True, dtype=None):
+        self.boolean_map = boolean_map
+        self.inplace = inplace
+        self.dtype = dtype
+    
+    def fit(self, X, y=None, **kwargs):
+        return None
+    
+    def transform(self, X, **kwargs):
+        if not self.inplace: X = X.copy()
+        for c in X.columns:
+            X[c].replace(self.boolean_map, inplace=True)
+            if self.dtype is not None: X[c] = X[c].astype(self.dtype)
+        return X
+    
+    def fit_transform(self, X, y=None, **kwargs):
+        return self.transform(X)
+
+
+    
+class StandardNaN:
+    """
+    Standartizes multiple NaN representations into an unique representation.
+
+    Ex: "", "Not Informed", "Not Available", etc -> np.NaN
+    """
+
+    def __init__(self, additional_nan_rep: list=[], std_nan_val=np.NaN, inplace: bool=True):
+        """
+        def __init__(self, additional_nan_rep: dict={}, std_nan_val=np.NaN, inplace: bool=True):
+        Class constructor
+
+        Input parameters:
+          - additional_nan_rep: a list with *additional* values that you wish to standartize
+            with an unique NaN representation. By default, the following values are already
+            included: 'na', 'nan', '', '<NA>', 'NÃO INFORMADO'.
+          - std_nan_val: the unique NaN representation that will be used instead of the 
+            representations passed.
+          - inplace: whether to work on a copy of the data or not.
+        """
+
+        #Original NaN reps
+        self.nan_map = {
+            'na' : std_nan_val,
+            'nan' : std_nan_val,
+            '' : std_nan_val,
+            '<NA>' : std_nan_val,
+            'NÃO INFORMADO' : std_nan_val,
+        }
+
+        #Adding any custom value brought by the user.
+        for v in additional_nan_rep:
+            self.nan_map[v] = std_nan_val
+
+        self.inplace = inplace
+    
+    def fit(self, X, y=None, **kwargs):
+        return None
+    
+    def transform(self, X, **kwargs):
+        if not self.inplace: X = X.copy()
+        return X.replace(self.nan_map)
+    
+    def fit_transform(self, X, y=None, **kwargs):
+        return self.transform(X)
+
+
+class SmartFloatCasting:
+    def __init__(self, orig_dec_sep=',', dest_dec_sep='.', inplace=True):
+        self.orig_dec_sep = orig_dec_sep
+        self.dest_dec_sep = dest_dec_sep
+        self.inplace = inplace
+    
+    def fit(self, X, y=None, **kwargs):
+        return None
+
+    def transform(self, X, **kwargs):
+        if not self.inplace: X = X.copy()
+        for c in X.columns:
+            X[c] = X[c].str.replace(self.orig_dec_sep, self.dest_dec_sep).astype(np.float64)
+        return X
+    
+    def fit_transform(self, X, y=None, **kwargs):
+        return self.transform(X)
