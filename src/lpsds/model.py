169f5cfd155796e7901ce8b45ea95656dc335fd7
Rodrigo Coura Torres <torres.rc@gmail.com>
2022-04-29 16:31:12 -0300

    Added create_validation_dataset

--- src/lpsds/model.py
+++ src/lpsds/model.py
@@ -27,3 +27,29 @@ def get_operation_model(model, X, y, metric=mean_squared_error, **kargv):
     res = np.array([metric(y, mod.predict(X), **kargv) for mod in model['estimator']])
     op_idx = np.argmin(res)
     return model['estimator'][op_idx]
+
+
+def create_validation_dataset(model, X, y, proba: bool=True, copy: bool=True):
+    """
+    def create_validation_dataset(model, X, y, proba: bool=True, copy: bool=True)
+
+    Create a validation dataset suitable for validate your model
+    when deployed to another operation environment.
+
+    Input parameters are:
+        - model: a sklearn-like model (pipeline, skorch, etc.)
+        - X: the input dataset
+        - y: the targets of X
+        - proba: if True, will call model.predict_proba. Otherwise, model.predict
+        - copy: whether to return a copy of the passed dataset.
+    
+    Returns:
+        A new dataset containing 2 new columns:
+            - y_true: containing the targets in y.
+            - y_pred: the model output for each sample in X.
+    """
+
+    if copy: df = X.copy()
+    df['y_true'] = y
+    df['y_pred'] = model.predict_proba(X) if proba else model.predict(X)
+    return df
