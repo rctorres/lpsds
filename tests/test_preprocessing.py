63f0c49972625f2b39de2a12e9caf1ed539b48c4
Rodrigo Coura Torres <torres.rc@gmail.com>
2022-05-05 17:23:28 -0300

    Added more tests

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -13,6 +13,62 @@ class TestStandardNaN:
         """Test object"""
         return StandardNaN()
 
+    def test_simple_case(self, obj):
+        """Test simple behavior"""
+
+        df = pd.DataFrame({
+            'a' : ['aaa', 'None', 'NA'], 
+            'b' : ['', 'bbb', 'NAN'],
+        })
+        ret = obj.fit_transform(df)
+        assert ret.a.iloc[0] == 'aaa'
+        assert np.isnan(ret.a.iloc[1])
+        assert np.isnan(ret.a.iloc[2])
+
+        assert np.isnan(ret.b.iloc[0])
+        assert ret.b.iloc[1] == 'bbb'
+        assert np.isnan(ret.b.iloc[2])
+
+
+    def test_nan_rep_addition(self):
+        """Test adding new nan rep"""
+
+        df = pd.DataFrame({
+            'a' : ['aaa', 'None', 'ccc'], 
+            'b' : ['', 'bbb', 'NAN'],
+        })
+
+        obj = StandardNaN(additional_nan_rep=['bbb', 'ccc'])
+        ret = obj.fit_transform(df)
+        assert ret.a.iloc[0] == 'aaa'
+        assert np.isnan(ret.a.iloc[1])
+        assert np.isnan(ret.a.iloc[2])
+
+        assert np.isnan(ret.b.iloc[0])
+        assert np.isnan(ret.b.iloc[1])
+        assert np.isnan(ret.b.iloc[2])
+
+
+    def test_std_nan_override(self):
+        """Testing setting a new NaN override"""
+
+        df = pd.DataFrame({
+            'a' : ['aaa', 'None', 'ccc'], 
+            'b' : ['', 'bbb', 'NAN'],
+        })
+
+        std_val = 'NEW'
+        obj = StandardNaN(additional_nan_rep=['aaa'], std_nan_val=std_val)
+        ret = obj.fit_transform(df)
+        assert ret.a.iloc[0] == std_val
+        assert ret.a.iloc[1] == std_val
+        assert ret.a.iloc[2] == 'ccc'
+
+        assert ret.b.iloc[0] == std_val
+        assert ret.b.iloc[1] == 'bbb'
+        assert ret.b.iloc[2] == std_val
+
+
     def test_case_insensitiveness(self, obj):
         """Test whether the transformation is case insensitive."""
 
@@ -29,3 +85,31 @@ class TestStandardNaN:
         for idx, c in df.iterrows():
             for col in df.columns:
                 assert np.isnan(np.nan)
+
+
+    def test_numeric_columns(self, obj):
+        """Test if the class can handle dataframe with numeric columns"""
+
+        df = pd.DataFrame({
+            'a' : [1, 2, np.nan], 
+            'b' : ['nan', 'To Keep', '<na>'],
+            'c' : [4.1, np.nan, 4.3],
+            'd' : ['To Keep', '<na>', '<Na>'],
+        })
+        ret = obj.fit_transform(df)
+        assert ret.a.iloc[0] == 1
+        assert ret.a.iloc[1] == 2
+        assert np.isnan(ret.a.iloc[2])
+
+        assert np.isnan(ret.b.iloc[0])
+        assert ret.b.iloc[1] == 'To Keep'
+        assert np.isnan(ret.b.iloc[2])
+
+        assert ret.c.iloc[0] == 4.1
+        assert np.isnan(ret.c.iloc[1])
+        assert ret.c.iloc[2] == 4.3
+
+        assert ret.d.iloc[0] == 'To Keep'
+        assert np.isnan(ret.d.iloc[1])
+        assert np.isnan(ret.d.iloc[2])
+
