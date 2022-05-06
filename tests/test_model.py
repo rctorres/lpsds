887bc4ab14caa891088f9f5177bb54b2be880a47
Rodrigo Coura Torres <torres.rc@gmail.com>
2022-05-06 09:55:28 -0300

    Declared all tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -6,11 +6,36 @@ import pytest
 
 class TestGetOperationModel:
     """Tests get_operation_model"""
-    pass
+    
+    def test_default_case(self):
+        """Test behavior with default values"""
+        pass
 
+    def test_metric_override(self):
+        """Test if overriding metrics is working"""
+        pass
+
+    def test_metrics_kwargs(self):
+        """Test kwargs for metric function"""
+        pass
 
 
 
 class TesteCreateValidationDataset:
     """Tests create_validation_dataset"""
-    pass
+
+    def test_default_case(self):
+        """Test behavior with default values"""
+        pass
+
+    def test_predict_proba(self):
+        """Tests predic probability case"""
+        pass
+
+    def test_copy_true(self):
+        """Test copy = True case for X and y"""
+        pass
+
+    def test_copy_false(self):
+        """Test copy = False case for X and y"""
+        pass
