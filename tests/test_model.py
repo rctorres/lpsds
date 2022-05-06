4a039cc6c2642c872826dd192bb8df36b8ab1e8c
Rodrigo Coura Torres <torres.rc@gmail.com>
2022-05-06 09:41:29 -0300

    Test classes declared

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,1 +1,16 @@
+"""model module tests"""
+
 import pytest
+
+
+
+class TestGetOperationModel:
+    """Tests get_operation_model"""
+    pass
+
+
+
+
+class TesteCreateValidationDataset:
+    """Tests create_validation_dataset"""
+    pass
