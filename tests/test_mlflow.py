1e77ed7b33ef2df6a240ab1b695f52356bea7b3d
Rodrigo Coura Torres <torres.rc@gmail.com>
2022-11-02 15:35:47 +0000

    Testing log_dataframe

--- tests/test_mlflow.py
+++ tests/test_mlflow.py
@@ -1,8 +1,10 @@
+import os
+import numpy as np
+import pandas as pd
 import pytest
 import mlflow
 import lpsds.metrics
-import numpy as np
-from lpsds.mlflow import log_statistics
+from lpsds.mlflow import log_statistics, log_dataframe
 
 class TestLogStatistics:
 
@@ -64,3 +66,39 @@ class TestLogStatistics:
         assert ret['auc']['auc_mean'] == 19.2
         assert ret['auc']['auc_err_min'] == 17.2
         assert ret['auc']['auc_err_max'] == 69.2
+
+
+class TestLogDataFrame():
+
+    @staticmethod
+    def assert_file_exists(temp_file_name, folder):
+        assert os.path.exists(temp_file_name)
+        assert folder == 'mlflow_test_folder'
+
+    @staticmethod
+    def assert_right_content(temp_file_name, folder):
+        df = pd.read_parquet(temp_file_name)
+        assert df.shape[0] == 3
+        assert df.shape[1] == 2
+
+        assert df.a.iloc[0] == 1
+        assert df.a.iloc[1] == 2
+        assert df.a.iloc[2] == 3
+
+        assert df.b.iloc[0] == 40
+        assert df.b.iloc[1] == 50
+        assert df.b.iloc[2] == 60
+
+
+    @pytest.fixture
+    def df(self):
+        return pd.DataFrame({'a' : [1,2,3], 'b' : [40,50,60]})
+
+    def test_file_exists(self, monkeypatch, df):
+        monkeypatch.setattr(mlflow, 'log_artifact', TestLogDataFrame.assert_file_exists)        
+        log_dataframe(df, 'filename.parquet', 'mlflow_test_folder')
+
+
+    def test_file_correct(self, monkeypatch, df):
+        monkeypatch.setattr(mlflow, 'log_artifact', TestLogDataFrame.assert_right_content)
+        log_dataframe(df, 'filename.parquet', 'mlflow_test_folder')
