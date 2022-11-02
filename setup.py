b9e89ce5b85190f8b3d8ff9cb44cdcfac028e070
Rodrigo Coura Torres <torres.rc@gmail.com>
2022-11-02 18:54:57 +0000

    Version update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="lpsds",
-    version="0.7.0",
+    version="0.8.0",
     author="Rodrigo Coura Torres",
     author_email="torres.rc@gmail.com",
     description="Helper functions for data science projects.",
