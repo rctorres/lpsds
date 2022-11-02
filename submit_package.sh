577c7aab9a29f27d85b92199248cbbe3055332bd
Rodrigo Coura Torres <torres.rc@gmail.com>
2022-11-02 17:28:59 +0000

    fix: repository to upload package

--- submit_package.sh
+++ submit_package.sh
@@ -3,6 +3,6 @@ echo "Creating lpsds installation packages"
 python -m build
 
 echo "Submitting newly packages to pypi"
-python -m twine upload --repository testpypi dist/*
+python -m twine upload dist/*
 
 echo "Submission completed!"
