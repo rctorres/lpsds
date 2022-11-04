40193d4b8176872dd56d9909ca6718eecda04502
Rodrigo Coura Torres <torres.rc@gmail.com>
2022-11-04 16:28:10 +0000

    Adding function to collect staged metrics

--- src/lpsds/operation.py
+++ src/lpsds/operation.py
@@ -76,3 +76,40 @@ def get_operation_model(cv_model: dict, cv_splits: list, X: Union[pd.DataFrame, 
     best_fold = cv_model[metric].argmin() if less_is_better else cv_model[metric].argmax()
     _, X_tst, _, y_tst, model = get_fold_data(cv_model, cv_splits, X, y_true, best_fold)
     return best_fold, X_tst, y_tst, model
+
+
+import pandas as pd
+
+
+def get_staged_metrics(cv, cv_splits, X, y_true, metrics_map, **kwargs):
+    aux_df_list = []
+    num_folds = len(cv_splits)
+    for fold in range(num_folds):
+        #Getting the test data and model for the fold
+        _, in_tst, _, targ_tst, model = get_fold_data(cv, cv_splits, X, y_true, fold)
+
+        #Pipelines do not implemet staged_predict. As a result, we need
+        #to apply the pre-processing manually. Probably there is a better way to do this...
+        for name, pp in model.steps[:-1]:
+            if hasattr(pp, 'transform'):
+                in_tst = pp.transform(in_tst)
+
+        #Collecting just the final estimator (i.e. the model being applied right after all the pre-processing pipeline).
+        estimator = model.steps[-1][1]
+
+        #Calculating the accuracy for each class w.r.t the number of stages.
+        for stage, out_tst in enumerate(estimator.staged_predict(in_tst)):
+            for metric_name, metric_function in metrics_map.items():
+                metric_value = metric_function(targ_tst, out_tst, **kwargs)
+                #Saving the results in an auxiliary dataframe
+                aux_df = pd.DataFrame({
+                    'Metric' : [metric_name],
+                    'Value' : [metric_value],
+                    'Stage' : [stage],
+                    'Fold' : [fold],
+                })
+                aux_df_list.append(aux_df)
+
+    #Creating the final dataframe in a format suitable for seaborn (long-format)
+    return pd.concat(aux_df_list)
+ 
