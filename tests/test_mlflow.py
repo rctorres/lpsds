2bea6b507ede6e846b7597329a87d48e9d4d3c74
Rodrigo Coura Torres <torres.rc@gmail.com>
2022-11-17 15:03:17 +0000

    Fixed log_statistics related tests

--- tests/test_mlflow.py
+++ tests/test_mlflow.py
@@ -4,9 +4,43 @@ import pandas as pd
 import pytest
 import mlflow
 import lpsds.metrics
-from lpsds.mlflow import log_statistics, log_dataframe
+from lpsds.mlflow import MLFlow
+from lpsds.utils import ObjectView
 
-class TestLogStatistics:
+class MLFlowBase:
+    @staticmethod
+    def mocked_active_run():
+        """
+        Mocks mlflow.active_run behavior
+        """
+        ret = ObjectView()
+        ret.info = ObjectView()
+        ret.info.run_id = 123
+        return ret
+    
+    @staticmethod
+    def mocked_get_run(run_id):
+        """
+        Mocks mlflow.active_run behavior
+        """
+        return 'my_run'
+
+    @staticmethod
+    def mocked_MlflowClient():
+        """
+        Mocks mlflow.active_run behavior
+        """
+        return ObjectView
+
+
+    @pytest.fixture(autouse=True)
+    def set_mlflow_patches(self, monkeypatch):
+        monkeypatch.setattr(mlflow, 'active_run', TestLogStatistics.mocked_active_run)
+        monkeypatch.setattr(mlflow, 'get_run', TestLogStatistics.mocked_get_run)
+        monkeypatch.setattr(mlflow.tracking, 'MlflowClient', TestLogStatistics.mocked_MlflowClient)
+
+
+class TestLogStatistics(MLFlowBase):
 
     @staticmethod
     def mocked_log_metrics(metrics_map):
@@ -42,28 +76,32 @@ class TestLogStatistics:
             'estimators' : np.array([1,2,3,4,5]),
             'test_auc' : np.array([10,2,30,4,50]), #mean=19.2, err_min=17.2, err_max=69.2
         }
+    
+    @pytest.fixture
+    def mlf_obj(self):
+        return MLFlow()
 
-    def test_only_2_returned(self, cv_map):
+    def test_only_2_returned(self, cv_map, mlf_obj):
         """
         Only 2 metrics must be returned
         """
-        ret = log_statistics(cv_map)
+        ret = mlf_obj.log_statistics(cv_map)
         assert len(ret) == 2
 
 
-    def test_metric_names(self, cv_map):
+    def test_metric_names(self, cv_map, mlf_obj):
         """
         Only SP and AUC must be returned
         """
-        ret = log_statistics(cv_map)
+        ret = mlf_obj.log_statistics(cv_map)
         assert 'sp' in ret
         assert 'auc' in ret
 
-    def test_metric_values(self, cv_map):
+    def test_metric_values(self, cv_map, mlf_obj):
         """
         Only metric values must be correct
         """
-        ret = log_statistics(cv_map)
+        ret = mlf_obj.log_statistics(cv_map)
         assert ret['sp']['sp_mean'] == 3
         assert ret['sp']['sp_err_min'] == 2
         assert ret['sp']['sp_err_max'] == 8
@@ -78,13 +116,13 @@ class TestLogStatistics:
         assert metric_map['sp_err_min'] == 2
         assert metric_map['sp_err_max'] == 8
 
-    def test_mlflow_submission(self, monkeypatch):
+    def test_mlflow_submission(self, monkeypatch, mlf_obj):
         met_map = {'test_sp' : np.array([1,2,3,4,5])} #mean = 3, err_min=2, err_max=8
         monkeypatch.setattr(mlflow, 'log_metrics', TestLogStatistics.assert_metrics_logged)
-        log_statistics(met_map)
+        mlf_obj.log_statistics(met_map)
     
 
-    def test_metrics_vector_logged(self, monkeypatch):
+    def test_metrics_vector_logged(self, monkeypatch, mlf_obj):
         self.metric_list = []
         def mocked_log_metric(key, value, step):
             self.metric_list.append((key, value, step))
@@ -92,7 +130,7 @@ class TestLogStatistics:
 
         met_map = {'test_sp' : np.array([1,2,3,4,5])} #mean = 3, err_min=2, err_max=8
         monkeypatch.setattr(mlflow, 'log_metric', mocked_log_metric)
-        log_statistics(met_map)
+        mlf_obj.log_statistics(met_map)
         for i, items in enumerate(self.metric_list):
             k,v,s = items
             assert k == 'sp'
@@ -127,11 +165,15 @@ class TestLogDataFrame():
     def df(self):
         return pd.DataFrame({'a' : [1,2,3], 'b' : [40,50,60]})
 
-    def test_file_exists(self, monkeypatch, df):
+    @pytest.fixture
+    def mlf_obj(self):
+        return MLFlow()
+
+    def test_file_exists(self, monkeypatch, df, mlf_obj):
         monkeypatch.setattr(mlflow, 'log_artifact', TestLogDataFrame.assert_file_exists)        
-        log_dataframe(df, 'filename.parquet', 'mlflow_test_folder')
+        mlf_obj.log_dataframe(df, 'filename.parquet', 'mlflow_test_folder')
 
 
-    def test_file_correct(self, monkeypatch, df):
+    def test_file_correct(self, monkeypatch, df, mlf_obj):
         monkeypatch.setattr(mlflow, 'log_artifact', TestLogDataFrame.assert_right_content)
-        log_dataframe(df, 'filename.parquet', 'mlflow_test_folder')
+        mlf_obj.log_dataframe(df, 'filename.parquet', 'mlflow_test_folder')
